27232f85338807d97855185e574a343e532a35a6
Jonathan Lee <jonlee50@hotmail.com>
2017-02-12 00:07:35 -0500

    COMP395_Assignment2_Spring

---
+++ Assets/MouseClick.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour {
+
+	bool click = false;
+	GameObject weight = null;
+
+	void Update() {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 point = getMousePoint ();
+
+			RaycastHit2D hit = Physics2D.Raycast(point, -Vector2.up);
+
+			if (hit.collider != null && hit.collider.tag == "weight") {
+				if (weight == null) {
+					weight = hit.collider.gameObject;
+				}
+				click = true;
+			}
+		}
+
+		if (click) {
+			Vector3 point = getMousePoint ();
+
+			weight.transform.position = new Vector3 (weight.transform.position.x, point.y, weight.transform.position.z);			
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			click = false;
+		}
+	}
+
+	Vector3 getMousePoint(){
+		float distance = 4.5f;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+		return ray.origin + (ray.direction * distance);
+	}
+}
---
+++ Assets/Spring.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+
+	public GameObject Anchor;
+	public GameObject spring;
+	public float K;
+    public Slider slider;
+
+	private float startingLangth = 0;
+	private Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		startingLangth = Anchor.transform.position.y - this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.K = slider.value;
+		float f = -this.K * (this.transform.position.y - startingLangth);
+		rb2d.AddForce(new Vector2(0, f));
+
+		float dis = 4 + ((this.transform.position.y -3)  * 0.5f);
+		float tra = (Anchor.transform.position.y - this.transform.position.y) * 0.5f;
+		spring.transform.position = new Vector3 (0, dis, 0);
+		spring.transform.localScale = new Vector3 (1, tra, 1);
+	}
+}
